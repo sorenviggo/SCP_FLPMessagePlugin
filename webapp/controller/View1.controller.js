1dd651ef3f4b1bb4047228bdb29133d4562491ab
Søren Hansen <sorenviggo@hotmail.com>
2019-09-10 14:20:56 +0000

    Barebone project from template 'SAPUI5 Application. Initial Commit.

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("dk.sorenviggo.scp.flp.plugin.SCP_FLPMessagePlugin.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"dk/sorenviggo/scp/flp/plugin/SCP_FLPMessagePlugin/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
